97da3534bc01d42478260bd916ced460b052c36a
Sergey K <skorotkov@yandex-team.ru>
2020-12-03 16:21:41 +0300

    cleanup

--- d3/src/d3.nim
+++ d3/src/d3.nim
@@ -1,6 +1,6 @@
 # This is just an example to get you started. A typical binary package
 # uses this file as the main entry point of the application.
-import strutils, sequtils
+import strutils
 
 proc getInput(file: bool): seq[string] =
   if file: return readFile("input.txt").strip.splitLines
